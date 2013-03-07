8f73b68f47de1dba7ad3a39411098c3a41322063
eric <eric@sager-np8662.(none)>
2013-03-07 01:15:06 -0500

    Added json to get_blog_trends

--- trackr/trackr/handlers.py
+++ trackr/trackr/handlers.py
@@ -6,19 +6,31 @@ from models import *
 
 ''' Add a new blog to our tracking list.'''	
 def add_blog(request):
-	blog_name = request.REQUEST['blog'] # Using .REQUEST instead of .POST for testing
-	b = Blog(host_name = blog_name)
-	b.save()
+	if (Blog.objects.filter(host_name = blog_name).exists() != True):
+		blog_name = request.REQUEST['blog'] # Using .REQUEST instead of .POST for testing
+		b = Blog(host_name = blog_name)
+		b.save()
 	return HttpResponse(blog_name)
 
 ''' Send trend data for some specific tumblr blog.'''
 def get_blog_trends(request, blog_name):
 	limit = request.GET['limit']
 	order = request.GET['order']
+	json = {"trending" : [], 
+	        "order": order, 
+	        "limit": limit}
 	if order == "Trending":
 		try:
 			blog_obj = Blog.objects.get(host_name=blog_name)
 			blog_likes = blog_obj.likes.order_by('-note_inc')[0:limit]
+			for post in blog_likes:
+				trending = {"url": post.url,
+				            "image": post.image,
+				            "date": post.date,
+				            "last_track": post.last_track,
+				            "last_count": post.note_count,
+				            "tracking": []}
+				json["trending"].append(trending)
 		except ObjectDoesNotExist:
 			return HttpResponse(404)
 	return HttpResponse(200)
--- trackr/trackr/models.py
+++ trackr/trackr/models.py
@@ -1,16 +1,26 @@
 # Define all the databases needed, each database will be created as
-# 'trackr_[class name], ex class Blog will be created as trackr_blog.
+# 'trackr_[class name], ex. class Blog will be created as trackr_blog.
+
 from django.db import models
+class Tracking(models.Model):
+    timestamp = models.DateField()
+    sequence = models.IntegerField()
+    increment = models.IntegerField()
+    count = models.IntegerField()
+
 class Post(models.Model):
     url = models.TextField()
-    date = models.TextField() # 
+    date = models.DateField() # 
+    last_track = models.DateField()
     image = models.TextField() # This is a url to the img
     note_count = models.IntegerField()
     note_inc = models.IntegerField()
     text = models.TextField()
+    tracking = models.OneToOneField(Tracking)
     
     def __unicode__(self):
         return self.url
+
 
 class Blog(models.Model):
     host_name = models.TextField(primary_key=True)
