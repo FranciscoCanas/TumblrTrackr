930181b837360810c9b625f08e059216b6047a8d
FranciscoCanas <mailfrancisco@gmail.com>
2013-03-10 16:25:25 -0400

    Prof test file

---
+++ golive.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python ~/CSC309/csc309-a2/trackr/manage.py runserver redwolf.cdf.toronto.edu:30945
---
+++ proftests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if test $# -ne 2
+then
+    echo "Usage: proftests.sh <host> <port>" >&2
+    exit 1
+fi
+
+echo Sending requests to: ${1}:${2}
+
+curl -X POST -d blog=fastcompany.tumblr.com http://${1}:${2}/blog
+curl -X POST -d blog=theatlantic.tumblr.com http://${1}:${2}/blog
+curl -X POST -d blog=condenasttraveler.tumblr.com http://${1}:${2}/blog
+curl -X POST -d blog=thisistheverge.tumblr.com http://${1}:${2}/blog
