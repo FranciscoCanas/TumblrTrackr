53ab477bcf6c29b519625c8271b6588e71351813
Wang Zi Zhou <eric.zizhou.w@gmail.com>
2013-03-10 15:55:07 -0400

    Fixed bug with url parsing dots

--- trackr/trackr/urls.py
+++ trackr/trackr/urls.py
@@ -10,17 +10,17 @@ import testquery
 urlpatterns = patterns('',
 	# ex: /blog
 	url(r'^blog$', handlers.add_blog),
-    # ex: /blog/deezcouches/trends
+        # ex: /blog/deezcouches/trends
 
-	url(r'^blog/(?P<blog_name>\w+)/trends$', handlers.get_blog_trends),
+	url(r'^blog/(?P<blog_name>.+)/trends$', handlers.get_blog_trends),
 
 	# ex: /blogs/trends
 	url(r'^blogs/trends$', handlers.get_trends),
 
 	# Used for manually starting a tracking go go machine sandwich
 	url(r'^track$', tracker.track), 
-    url(r'^testq$', testquery.testquery),
-    url(r'^ping$', handlers.ping) 
+        url(r'^testq$', testquery.testquery),
+        url(r'^ping$', handlers.ping) 
 )
 
 
