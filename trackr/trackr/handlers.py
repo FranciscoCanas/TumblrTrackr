1c2f122942b53875f3d318df048e357fa4a12600
Milic Sasa <c0milics@cdf.toronto.edu>
2013-03-11 12:12:23 -0400

    Combined get_trends and get_blog_trends into one function.

--- trackr/trackr/handlers.py
+++ trackr/trackr/handlers.py
@@ -5,91 +5,79 @@ from django.core.exceptions import ObjectDoesNotExist
 from trackr.models import *
 import tracker
 
-''' Add a new blog to our tracking list.''' 
+
 def add_blog(request):
+	''' Add a new blog to our tracking list.''' 
+	
 	blog_name = request.REQUEST['blog'] # Using .REQUEST instead of .POST for testing
-	if (Blog.objects.filter(host_name = blog_name).exists() != True):
+	if (not Blog.objects.filter(host_name = blog_name).exists()):
 		b = Blog(host_name = blog_name)
 		b.save()
-	tracker._request_likes(blog_name)
-	return HttpResponse(blog_name)
+	# We track a blog for the first time as soon as an add blog request
+	# is made for that particular blog.
+	tracker.request_likes(blog_name)
+	
+	return HttpResponse(content=blog_name, status=200)
 
-''' Send trend data for some specific tumblr blog.'''
-def get_blog_trends(request, blog_name):
+def get_trends(request, blog_name=None):
+	''' Send trend data for some specific tumblr blog ,
+	or all blogs if blog_name=None.'''
+	
 	limit = request.GET.get('limit', 10)
-	order = request.GET.get('order', None)
+	# Order must be specified.
+	order = request.GET.get('order')
 	if order == None:
-				return HttpResponse(content="Please specify order",status=404)	
-	blog_obj = Blog.objects.get(host_name = blog_name) #Gets a Blog object in 			database with blog_name as host_name
-	result = {"trending" : [], 
+		return HttpResponse(content="Order not specified.", status=404)	
+	
+	#Initialize a JSON object that will contain trends.
+	result = {order.lower() : [], 
 	        "order": order, 
-	        "limit": limit} #Initializes a JSON object to track all posts liked by a blog
+	        "limit": limit}
+	
 	if order == "Trending":
-		blog_likes = blog_obj.likes.order_by('-note_inc')[0:limit] #QuerySet of liked posts with most note_count
-		for post in blog_likes:
-			trending = {"url": post.url,
-			            "image": post.image,
-			            "text": post.text,
-			            "date": '{:%Y-%m-%d %H:%M:%S %Z}'.format(post.date),
-			            "last_track": '{:%Y-%m-%d %H:%M:%S} EST'.format(post.last_track),
-			            "last_count": post.note_count,
-			            "tracking": get_timestamps(post)}
-			result["trending"].append(trending)
+		orderby = '-note_inc'
 	elif order == "Recent":
-		blog_likes = blog_obj.likes.order_by('-date')[0:limit] #QuerySet of liked posts with most recent tracking
-		for post in blog_likes:
-			trending = {"url": post.url,
-			            "image": post.image,
-			            "text": post.text,
-			            "date": '{:%Y-%m-%d %H:%M:%S %Z}'.format(post.date),
-			            "last_track": '{:%Y-%m-%d %H:%M:%S} EST'.format(post.last_track),
-			            "last_count": post.note_count,
-			            "tracking": get_timestamps(post)}
-			result["trending"].append(trending)
+		orderby = '-date'
+	else:
+		return HttpResponse(content=order + " is not a valid order.", status=404)	
+	
+	if blog_name:
+		# Get liked posts for this specific blog.
+		blog_obj = Blog.objects.get(host_name = blog_name)
+		posts = blog_obj.likes.order_by(orderby)[0:limit]
+	else:	
+		# Get liked posts for all blogs.
+		posts = Post.objects.all().order_by(orderby)[0:limit]
+	
+	# QuerySet of liked posts with sorted by either '-note_inc' or 'date',
+	# depending on order parameter.
+	for post in posts:
+		p = {"url": post.url,
+		     "image": post.image,
+		     "text": post.text,
+		     "date": '{:%Y-%m-%d %H:%M:%S %Z}'.format(post.date),
+		     "last_track": '{:%Y-%m-%d %H:%M:%S} EST'.format(post.last_track),
+		     "last_count": post.note_count,
+		     "tracking": get_timestamps(post)}
+		result[order.lower()].append(p)
+
 	return HttpResponse(content=json.dumps(result), status=200)
 
 def get_timestamps(post):
-	'''Return a list of timestamp dictionaries.'''
+	'''Return a list of timestamp dicts.'''
+	
 	lst = []
 	for t in Tracking.objects.filter(post = post.id):
-		tdict = {"timestamp" : '{:%Y-%m-%d %H:%M:%S} EST'.format(t.timestamp),
+		tdict = {"timestamp" : '{:%Y-%m-%d %H:%M:%S %Z}'.format(t.timestamp),
 		         "sequence" : t.sequence,
-		         "increment" : t.increment,
-		         "count" : t.count}
+		         "increment" : post.note_inc,
+		         "count" : post.note_count}
 		lst.append(tdict)
+		
+	# Return timestamps sorted by descending sequence number
 	return sorted(lst, key=lambda k: k['sequence'], reverse=True) 
 	
-def get_trends(request):
-	''' Send trending data for all blogs.'''
-	
-	limit = request.GET.get('limit', 10)
-	order = request.GET.get('order', None)
-	if order == None:
-		return HttpResponse(status=404)
-	
-	result = {"trending" : [], 
-	        "order": order, 
-	        "limit": limit} #Initializes a JSON object to track all posts liked by a blog
-	if order == 'Trending':
-		order_by = '-note_inc'
-	elif order == 'Recent':
-		order_by = '-date'
-	else:
-		return HttpResponse(status=404)
-	posts = Post.objects.all().order_by(order_by)[0:limit]
-	for post in posts:
-		trending = {"url": post.url,
-	                    "text": post.text,
-	                    "image": post.image,
-	                    "date": '{:%Y-%m-%d %H:%M:%S %Z}'.format(post.date),
-	                    "last_track":'{:%Y-%m-%d %H:%M:%S} EST'.format(post.last_track),
-	                    "last_count": post.note_count,
-	                    "tracking": get_timestamps(post)}
-		result["trending"].append(trending)
-
-	return HttpResponse(content=json.dumps(result), status=200)
-	
 def ping(request):
-	return HttpResponse(200);
+	return HttpResponse(content="PING!", status=200);
 
 
